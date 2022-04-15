11c149e3f2c037ba1a1cbee579eff0e11c9a4a9f
ceyhun01q <ceyhunqasimov999@gmil.com>
2022-04-16 03:38:24 +0400

    new things

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const skl3 = document.querySelector(".skl3")
 const skl4 = document.querySelector(".skl4")
 
 
-button.addEventListener("click",go)
+ button.addEventListener("click",go)
+// let button1 = document.querySelector('form .btn1')
+// button1.addEventListener("mauseover",()=>{
+//     button1.style.color = "red"
+// })
 
 function go (){
     tasklistbox.style.display = "block"
@@ -21,34 +25,47 @@ function go (){
     li.innerText = input.value;
     tasklistbox.appendChild(li)
 
-
     if(input.value == ""){
         alert("nese yaz")
     }
     else{
-        input.value = ""
+    input.value = ""
     let deleteButton = document.createElement("div")
     deleteButton.classList.add("deleteButton")
     li.appendChild(deleteButton)
-    deleteButton.innerHTML = "&times;"
+    deleteButton.innerHTML = '<img src = "./image/Group7.png" witdh = 20px height = 20px>'
+
     deleteButton.addEventListener("click",function(){
-        tasklistbox.removeChild(li)
-
-
+    tasklistbox.removeChild(li)
     })
 
     }
-    
-}
 
+}
 
 const  azerbaycan = document.querySelector(".a");
 
 function fnClick (){
-    skl1.style.display = "block";
-    skl2.style.display = "none";
+    azerbaycan.addEventListener("mouseover",hover);
+    azerbaycan.addEventListener("mouseout",hover1);
+    function hover(){
+        
+        skl2.style.display = "block"
+        skl1.style.display = "none"
+        skl3.style.display = "none"
+        skl4.style.display = "none"
+    };
+    function hover1 (){
+            
+            skl1.style.display = "block"
+            skl2.style.display = "none"
+            skl3.style.display = "none"
+            skl4.style.display = "none"
+        };
+    
+     skl1.style.display = "block";
      skl3.style.display = "none";
-
+     
      let list, i, switching, b, shouldSwitch;
      list = document.querySelector(".tasklistbox");
      switching = true;
@@ -60,23 +77,39 @@ function fnClick (){
             if (b[i].innerText.toLowerCase()> b[i + 1].innerText.toLowerCase()) {
               shouldSwitch = true;
                break;
-             }
-        }
+             };
+        };
         if (shouldSwitch) {
             b[i].parentNode.insertBefore(b[i + 1], b[i]);
             switching = true;
-         }
-    }
+         };
+    };
 
     azerbaycan.removeEventListener("click",fnClick);
     azerbaycan.addEventListener("click", onClick);
 
-}
+};
 
 function onClick (){
     skl1.style.display = "none";
-    skl2.style.display = "none";
     skl3.style.display = "block";
+    azerbaycan.addEventListener("mouseover",hover);
+    azerbaycan.addEventListener("mouseout",hover1);
+    function hover(){
+        skl4.style.display = "block"
+        skl3.style.display = "none"
+        skl1.style.display = "none"
+        skl2.style.display = "none"
+     };
+
+     function hover1 (){
+         
+        skl4.style.display = "none"
+        skl3.style.display = "block"
+        skl1.style.display = "none"
+        skl2.style.display = "none"
+
+            };
 
     let list, i, switching, b, shouldSwitch;
     list = document.querySelector(".tasklistbox");
@@ -89,15 +122,48 @@ function onClick (){
             if (b[i + 1].innerText.toLowerCase()>b[i].innerText.toLowerCase()) {
               shouldSwitch = true;
               break;
-           }
-       }
+           };
+       };
        if (shouldSwitch) {
            b[i].parentNode.insertBefore(b[i + 1], b[i]);
            switching = true;
-          }
-    }
+          };
+    };
     azerbaycan.removeEventListener("click", onClick);
-    azerbaycan.addEventListener("click",fnClick)
+    azerbaycan.addEventListener("click",fnClick);
+    azerbaycan.addEventListener("mouseover",hover);
+    azerbaycan.addEventListener("mouseout",hover1);
+};
+azerbaycan.addEventListener("click",fnClick);
+
+button.addEventListener("mouseover", myScript);
+button.addEventListener("mouseout", myScript1);
+function myScript (){
+    button.style.background = "red"
+    button2.style.background= "yellow"
+    button.style.border = "solid 1px"
+    button2.style.border = "solid 1px"
+
+};
+function myScript1 (){
+    button.style.background= "#833AE0"
+    button2.style.background= "#9953F1"
+};
+function hover(){
+    skl1.style.display = "none"
+    skl2.style.display = "block"
+ };
+function hover1 (){
+    skl1.style.display = "block"
+    skl2.style.display = "none"
+};
+
+let i = 1;
+while(i=fnClick()){
+azerbaycan.addEventListener("mouseover",hover);
+azerbaycan.addEventListener("mouseout",hover1);
+
+
 }
-azerbaycan.addEventListener("click", fnClick);
+
 
